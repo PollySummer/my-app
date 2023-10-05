6de69351413b89e97d4d044cbf464297b807807a
PollySummer <polinaivanenko.cat@gmail.com>
2023-10-05 17:09:59 +0300

    added delete button

--- src/Body/DataPage.jsx
+++ src/Body/DataPage.jsx
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { setSelectedOrder, setShowModal } from '../services/stateServices';
+import { setSelectedOrder, setShowModal, setHiddenRows } from '../services/stateServices';
 import Table from 'react-bootstrap/Table';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
@@ -9,7 +9,17 @@ function DataPage() {
     const selectedOrder = useSelector((state) => state.selectedOrder);
     const shipmentData = useSelector((state) => state.shipmentData);
     const showModal = useSelector((state) => state.showModal);
+    const hiddenRows = useSelector((state) => state.hiddenRows);
     const dispatch = useDispatch();
+
+
+    const toggleRowVisibility = (index) => {
+        if (hiddenRows.includes(index)) {
+            dispatch(setHiddenRows(hiddenRows.filter((rowIndex) => rowIndex !== index)));
+        } else {
+            dispatch(setHiddenRows([...hiddenRows, index]));
+        }
+    };
 
     if (!Array.isArray(shipmentData)) {
         return <div>No shipment data available.</div>;
@@ -47,23 +57,27 @@ function DataPage() {
                 </thead>
                 <tbody>
                     {data.map((order, index) => (
-                        <tr key={index}>
-                            <td>{order.orderNo}</td>
-                            <td>{order.date}</td>
-                            <td>{order.customer}</td>
-                            <td>{order.consignee}</td>
-                            <td>{order.trackingNo}</td>
-                            <td>{order.status}</td>
-                            <td>
-                                <Button variant="info" onClick={() => handleInfoClick(order)}>Info</Button>
-                                <Button variant="danger">X</Button>{' '}
-                            </td>
-                        </tr>
+                        hiddenRows.includes(index) ? null : (
+                            <tr key={index}>
+                                <td>{order.orderNo}</td>
+                                <td>{order.date}</td>
+                                <td>{order.customer}</td>
+                                <td>{order.consignee}</td>
+                                <td>{order.trackingNo}</td>
+                                <td>{order.status}</td>
+                                <td>
+                                    <div className='ms-3'>
+                                        <Button variant="info" onClick={() => handleInfoClick(order)}>&#8520;</Button>
+                                        <Button variant="danger" onClick={() => toggleRowVisibility(index)}>X</Button>
+                                    </div>
+                                </td>
+                            </tr>
+                        )
                     ))}
                 </tbody>
             </Table>
 
-            <OrderWindow show={showModal} onHide={() => dispatch( setShowModal(false))} selectedOrder={selectedOrder} />
+            <OrderWindow show={showModal} onHide={() => dispatch(setShowModal(false))} selectedOrder={selectedOrder} />
         </Container>
     );
 }
--- src/services/stateServices.js
+++ src/services/stateServices.js
@@ -4,15 +4,16 @@ const initialState = {
     shipmentData: null,
     selectedOrder: false,
     showModal: false,
+    hiddenRows: [],
 };
 
 export const setShipmentData = createAction('setShipmentData');
 export const setSelectedOrder = createAction('setSelectedOrder');
 export const setShowModal = createAction('setShowModal');
+export const setHiddenRows = createAction('setHiddenRows');
 
 const reducer = createReducer(initialState, (builder) => {
-    builder.
-        addCase(setShipmentData, (state, action) => {
+    builder.addCase(setShipmentData, (state, action) => {
             state.shipmentData = action.payload;
         })
         .addCase(setSelectedOrder, (state, action) => {
@@ -20,6 +21,10 @@ const reducer = createReducer(initialState, (builder) => {
         })
         .addCase(setShowModal, (state, action) => {
             state.showModal = action.payload;
+        }
+        )
+        .addCase(setHiddenRows, (state, action) => {
+            state.hiddenRows = action.payload;
         }
         )
 });

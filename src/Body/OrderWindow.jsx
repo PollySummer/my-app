d664ac5c74a203bc463605fa2d11725aaa62ec1c
PollySummer <polinaivanenko.cat@gmail.com>
2023-10-05 17:23:29 +0300

    added some form design

--- src/Body/OrderWindow.jsx
+++ src/Body/OrderWindow.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
@@ -9,7 +8,7 @@ import { useSelector } from 'react-redux';
 function OrderWindow({ show, onHide }) {
   const selectedOrder = useSelector((state) => state.selectedOrder);
   return (
-    <Modal show={show} onHide={onHide}>
+    <Modal size="lg" show={show} onHide={onHide}>
       <Modal.Header closeButton>
         <Modal.Title>Order Details</Modal.Title>
       </Modal.Header>
@@ -19,7 +18,7 @@ function OrderWindow({ show, onHide }) {
             <Row>
               <Col>
                 <Form.Label>OrderNo</Form.Label>
-                <Form.Control type="text" value={selectedOrder.orderNo} readOnly />
+                <Form.Control className='mb-4' type="text" value={selectedOrder.orderNo} readOnly />
               </Col>
               <Col>
                 <Form.Label>Date</Form.Label>
@@ -33,7 +32,7 @@ function OrderWindow({ show, onHide }) {
             <Row>
               <Col>
                 <Form.Label>Consignee</Form.Label>
-                <Form.Control type="text" value={selectedOrder.consignee} readOnly />
+                <Form.Control className='mb-3' type="text" value={selectedOrder.consignee} readOnly />
               </Col>
               <Col>
                 <Form.Label>TrackingNo</Form.Label>
